fcbcc8be0935437ed812ac81cb626861bbbba474
miguel medina <mike_wiii@hotmail.com>
2020-09-05 14:52:44 -0500

    test de imagenes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var imagen = document.getElementById('Icons').
+addEventListener("click", function(event)
+{
+    console.log("Diste click a la imagen");
+    console.log(imagen);
+    var valor = document.getElementById('Atleta').value;
+    console.log(valor)
+},false);
+
+document.getElementsByName('username').innerHTML= valor;
