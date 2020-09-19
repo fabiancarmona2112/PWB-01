a78ad6b3e47ee0128f4a8f96156e1c786e0fbf6c
dyMed <mike_wiii@hotmail.com>
2020-09-19 14:48:09 -0500

    ediciones

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-var imagen = document.getElementById('Icons').
-addEventListener("click", function(event)
-{
-    console.log("Diste click a la imagen");
-    console.log(imagen);
-    var valor = document.getElementById('Atleta').value;
-    console.log(valor)
-},false);
 
-document.getElementsByName('username').innerHTML= valor;
+function getName(pasoPorValor){    
+    console.log(pasoPorValor);
+}
+
+getName("Valor");
