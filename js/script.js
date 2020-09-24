a667d93badf831f03ec84f8f54613efb7bd4e894
miguel medina <mike_wiii@hotmail.com>
2020-09-23 23:33:41 -0500

    navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-//document.write('<div> <img src="userIcon/atleta.png"> <h1> Titulo </h1> </div>');
+const navSlided = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-list');
+    const navList = document.querySelectorAll('.nav-list li');
+
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+    });
+
+    navList.forEach((link, index)=>{
+        link.style.animation = `navLinstFade 0.5s ease fordwards ${index/7+2}s`;
+        console.log(index);
+    });
+}
+    navSlided();
